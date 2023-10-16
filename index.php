8cd8365bce129e8010b05645dadabcf6ba043eb4
Henri Sarapuu <115624166+Hentri@users.noreply.github.com>
2023-10-16 10:29:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,39 @@
+<!DOCTYPE html>
 <html>
-  <H1>see on testimiseks</H1>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Website Title</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to Your Website</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <h2>About Us</h2>
+            <p>Insert your about us content here.</p>
+        </section>
+        <section>
+            <h2>Services</h2>
+            <p>Explore our services and what we offer.</p>
+        </section>
+        <section>
+            <h2>Contact Us</h2>
+            <p>Contact information and a contact form can go here.</p>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; <?php echo date('Y'); ?> Your Website Name. All rights reserved.</p>
+    </footer>
+</body>
 </html>
-
