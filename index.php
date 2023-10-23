632cd18b244f6d116fed3de26217f3bd55167da4
Henri Sarapuu <115624166+Hentri@users.noreply.github.com>
2023-10-23 10:38:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Website Title</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    <style>
-        .about-box {
-            background-color: #f7f7f7;
-            border: 1px solid #ddd;
-            padding: 20px;
-            border-radius: 5px;
-            margin: 20px;
-        }
-    </style>
+    <title>Cristiano Ronaldo</title>
 </head>
 <body>
-    <header>
-        <h1>Welcome to Your Website</h1>
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
-    <main>
-        <section>
-            <div class="about-box">
-                <h2>About Us</h2>
-                <p>Insert your about us content here. You can provide a brief introduction to your website or yourself in this section.</p>
-            </div>
-        </section>
-        <section>
-            <h2>Services</h2>
-            <p>Explore our services and what we offer.</p>
-        </section>
-        <section>
-            <h2>Contact Us</h2>
-            <p>Contact information and a contact form can go here.</p>
-        </section>
-    </main>
-    <footer>
-        <p>&copy; <?php echo date('Y'); ?> Your Website Name. All rights reserved.</p>
-    </footer>
+    <h1>Welcome to the  best Cristiano Ronaldo Fan Page</h1>
+    <p>This is an example of a web page with an image of Cristiano Ronaldo:</p>
+    <img src="https://en.wikipedia.org/wiki/Cristiano_Ronaldo#/media/File:Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis,_September_2023_(cropped).jpg" alt="Cristiano Ronaldo">
 </body>
 </html>
